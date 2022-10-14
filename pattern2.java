17a0ce1ca3fffb2ee2ea05028a37e71395624407
Pavani Padyanjalee <98758225+Pavani-Padyanjalee@users.noreply.github.com>
2022-10-14 19:28:23 +0530

    Add files via upload

---
+++ pattern2.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.pattern;
+
+/**
+ *
+ * @author ICT5
+ */
+public class pattern2 {
+    public static void main(String[]args){
+        
+ int x=5;      
+ 
+        for (int i=4; i>=0 ; i--)  
+    {  
+        for (int j=0; j<=i; j++)  
+    {  
+        System.out.print("*" + " ");  
+    }  
+        System.out.println();  
+}}}
